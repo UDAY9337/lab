073a328c28b3844196b42ebb643df20702c7f820
UDAY9337 <106107028+UDAY9337@users.noreply.github.com>
2022-06-24 11:37:22 +0530

    Update multiplication.java

--- multiplication.java
+++ multiplication.java
@@ -1,10 +1,11 @@
+//program to print multiplication of two integers
 class multiplication
 {
   public static void main(String[]args)
   {
-  int  a=10;
-  int b=12;
+  int  a=10; //1st integers
+  int b=12; //2nd integers
   int mul=a*b;
-    System.out.println("multiplication of the number is "+mul);
+    System.out.println("multiplication of the number is "+mul); //output
   }
 }
