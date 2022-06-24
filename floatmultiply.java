a23c717aa32e6caf800b9a5c23bec48ad0214004
UDAY9337 <106107028+UDAY9337@users.noreply.github.com>
2022-06-24 11:34:07 +0530

    Update floatmultiply.java

--- floatmultiply.java
+++ floatmultiply.java
@@ -1,10 +1,11 @@
+//program to print two floating point number
 class floatmultiply
 {
   public static void main(String[]args)
   {
-  float a=1.05f;
-  float b=12.50f;
-  float mul=a*b;
-    System.out.println("the multiplication is "+mul);
+  float a=1.05f; //1st floating number
+  float b=12.50f; //2nd floating number
+  float mul=a*b; //multiplication
+    System.out.println("the multiplication is "+mul); //output
   }
 }
