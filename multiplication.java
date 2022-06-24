ebe001661e6ce36ccb494235d0b58a7f12648d36
UDAY9337 <106107028+UDAY9337@users.noreply.github.com>
2022-06-24 11:36:20 +0530

    Add files via upload

---
+++ multiplication.java
@@ -0,0 +1,10 @@
+class multiplication
+{
+  public static void main(String[]args)
+  {
+  int  a=10;
+  int b=12;
+  int mul=a*b;
+    System.out.println("multiplication of the number is "+mul);
+  }
+}
