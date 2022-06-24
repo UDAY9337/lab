f3f3952d26b124a08a6b631af355b717e846cb89
UDAY9337 <106107028+UDAY9337@users.noreply.github.com>
2022-06-24 11:31:32 +0530

    Update addition.java

--- addition.java
+++ addition.java
@@ -1,10 +1,11 @@
+//java program to add 2 numbers
 class addition
 {
   public static void main(String[]args)
   {
-  int  a=10;
-  int b=12;
+  int  a=10; // 1st number
+  int b=12; // 2nd number
   int add=a+b;
-    System.out.println("sum of the number is "+add);
+    System.out.println("sum of the number is "+add); //output
   }
 }
