efb8df9de40fb34ae0814e3b4d0a955f8a5bc943
UDAY9337 <106107028+UDAY9337@users.noreply.github.com>
2022-07-15 21:10:25 +0530

    Add files via upload

---
+++ Advancedarithematic.java
@@ -0,0 +1,26 @@
+package com;
+import java.util.Scanner;
+ public interface AdvancedArithmetic {
+	int divisorsum(int n);
+}
+ class MyCalculator implements AdvancedArithmetic {
+	 public int divisorsum(int n) {
+		 int sum = 1;
+		 for (int i=2;i<=n;i++) {
+			 if (n%i == 0) {
+				 sum=sum+i;
+			 }
+		 }
+		 return sum;
+	 }
+	 class Solution
+	 {
+		 public static void main(String args[]) {
+			 Scanner t=new Scanner(System.in);
+			 System.out.print("Enter the number:");
+			 int n = t.nextInt();
+			 MyCalculator c=new MyCalculator();
+			 System.out.println("I implemented:AdvancedArithmetic "+c.divisorsum(n));
+		 }
+	 }
+ }
